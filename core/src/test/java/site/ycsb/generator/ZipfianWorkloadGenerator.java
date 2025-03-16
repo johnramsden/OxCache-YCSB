626a004d60468c849a004d225fe73cf9f43260df
John Ramsden <john@ubc.ramsdenj.ca>
2025-03-16 14:58:47 -0700

    Update generator to make values within correct range
    Print to .bin and .txt

--- core/src/test/java/site/ycsb/generator/ZipfianWorkloadGenerator.java
+++ core/src/test/java/site/ycsb/generator/ZipfianWorkloadGenerator.java
@@ -32,7 +32,7 @@ public class ZipfianWorkloadGenerator {
   }
 
   public ZipfianWorkloadGenerator(int chunk_size, distributionType distributionType, int working_set_ratio, int zone_size,
-                          int num_zones, int iterations, String directory, boolean isBinaryFile) {
+                                  int num_zones, int iterations, String directory, boolean isBinaryFile) {
     this.chunk_size = chunk_size;
     this.distributionType = distributionType;
     this.working_set_ratio = working_set_ratio;
@@ -48,48 +48,68 @@ public class ZipfianWorkloadGenerator {
   public void generateWorkloadFile() throws IOException {
     System.out.println("Creating " + this.file_name);
     new File(directory).mkdirs();
-    File output = new File(this.directory + this.file_name);
-    FileOutputStream stream = new FileOutputStream(output);
 
-    // Generate the data IDs that will be referenced in the workload
-    int total_ids = total_chunks * working_set_ratio;
-    int[] db = new int[total_ids];
-    Random rand = new Random();
-    for (int i = 0;i < total_ids; i++) {
-      db[i] = rand.nextInt();
+    File outputBinary = new File(this.directory + this.file_name + ".bin");
+    File outputText = new File(this.directory + this.file_name + ".txt");
+
+    try (
+        FileOutputStream binaryStream = new FileOutputStream(outputBinary);
+        // Use a BufferedWriter for the text file
+        java.io.BufferedWriter textWriter = new java.io.BufferedWriter(
+            new java.io.FileWriter(outputText))
+    ) {
+      // Generate the data IDs that will be referenced in the workload
+      int total_ids = total_chunks * working_set_ratio;
+
+      // Prepare a ByteBuffer for binary output
+      ByteBuffer arr = ByteBuffer.allocate(4 * iterations);
+
+      long min = 0;
+      long max = total_ids - 1;
+
+      Random rand = new Random();
+
+      switch (this.distributionType) {
+        case ZIPFIAN:
+          ZipfianGenerator zipfian = new ZipfianGenerator(min, max);
+          for (int i = 0; i < iterations; i++) {
+            int rnd = Math.toIntExact(zipfian.nextValue());
+            assertFalse(rnd < min);
+            assertFalse(rnd > max);
+
+            arr.putInt(rnd);
+
+            // Also write to the text file
+            textWriter.write(i + ": " + rnd);
+            textWriter.newLine();
+          }
+          break;
+
+        case SEQUENTIAL:
+          for (int i = 0; i < iterations; i++) {
+            arr.putInt(i);
+
+            textWriter.write(i + ": " + i);
+            textWriter.newLine();
+          }
+          break;
+
+        case UNIFORM:
+          for (int i = 0; i < iterations; i++) {
+            int rnd = rand.nextInt(total_ids);
+            arr.putInt(rnd);
+
+            textWriter.write(i + ": " + rnd);
+            textWriter.newLine();
+          }
+          break;
+      }
+
+      binaryStream.write(arr.array());
+
     }
 
-    // Generate the workload
-    ByteBuffer arr = ByteBuffer.allocate(4 * iterations);
-    long min = 0;
-    long max = total_ids - 1;
-
-    switch (this.distributionType) {
-      case ZIPFIAN:
-        ZipfianGenerator zipfian = new ZipfianGenerator(min, max);
-        for (int i = 0; i < iterations; i++) {
-          long rnd = zipfian.nextValue();
-          assertFalse(rnd < min);
-          assertFalse(rnd > max);
-          arr.putInt(db[(int) rnd]);
-        }
-        break;
-      case SEQUENTIAL:
-        for (int i = 0; i < iterations; i++) {
-          long rnd = i % total_ids;
-          arr.putInt(db[(int) rnd]);
-        }
-        break;
-      case UNIFORM:
-        for (int i = 0; i < iterations; i++) {
-          long rnd = rand.nextInt(total_ids);
-          arr.putInt(db[(int) rnd]);
-        }
-        break;
-    }
-
-
-    stream.write(arr.array());
-    stream.close();
+    System.out.println("Generated files: " + outputBinary.getAbsolutePath() + ", " + outputText.getAbsolutePath());
   }
+
 }
