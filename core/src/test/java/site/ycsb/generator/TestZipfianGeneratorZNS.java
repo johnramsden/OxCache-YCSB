043b2636ce2295b347cdfc20e87a4b13a836749f
John Ramsden <john@ubc.ramsdenj.ca>
2025-08-22 21:56:18 -0700

    Add chunked evict config

--- core/src/test/java/site/ycsb/generator/TestZipfianGeneratorZNS.java
+++ core/src/test/java/site/ycsb/generator/TestZipfianGeneratorZNS.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 import static org.testng.AssertJUnit.assertFalse;
@@ -19,10 +20,17 @@ public class TestZipfianGeneratorZNS {
     // final int zone_size = 1077 * 1024 * 1024; // ZNS
    final int zone_size = 1024 * 1024 * 1024; // SSD
 
+    int chunks_in_64K = (int) (zone_size / Math.pow(2, 16));
+    int chunks_in_512M = (int) (zone_size / Math.pow(2, 29));
+
     final String directory = "./target/workloads/";
     final Workload[] workloads = {
-        new Workload((int) Math.pow(2, 16), 40632, 19660800, 1, 2, "promotional", 80),
-        new Workload((int) Math.pow(2, 29), 5413781, 6_000, 8, 16, "promotional", 200),
+        // Promotional
+        new Workload((int) Math.pow(2, 16), 40632, 19660800, 1, 2, null, null, "promotional", 80),
+        new Workload((int) Math.pow(2, 29), 5413781, 6_000, 8, 16, null, null, "promotional", 200),
+        // Chunk
+        new Workload((int) Math.pow(2, 16), 40632, 19660800, chunks_in_64K, chunks_in_64K*2, Optional.of(chunks_in_64K), Optional.of(chunks_in_64K / 2), "chunk", 80),
+        new Workload((int) Math.pow(2, 29), 5413781, 6_000, chunks_in_512M*8, chunks_in_512M*16, Optional.of(chunks_in_512M*8), Optional.of(chunks_in_512M*4), "chunk", 200),
     };
 
     final int[] workingSetRatios = new int[]{10, 2};
--- core/src/test/java/site/ycsb/generator/Workload.java
+++ core/src/test/java/site/ycsb/generator/Workload.java
@@ -1,30 +1,43 @@
 package site.ycsb.generator;
 
+import java.util.Optional;
+
 public class Workload {
     public int chunkSize;
     public int latency;
     public int evictThresholdHigh;
     public int evictThresholdLow;
+    public Optional<Integer> cleanThresholdHigh;
+    public Optional<Integer> cleanThresholdLow;
     public int iterations;
     public int zones;
     public String evictionType;
 
     public String toString() {
-        return "chunk_size=" + chunkSize +
+        String ret = "chunk_size=" + chunkSize +
                ",latency=" + latency +
                ",evict_high=" + evictThresholdHigh +
                ",evict_low=" + evictThresholdLow +
                ",eviction=" + evictionType +
                ",iterations=" + iterations +
                ",n_zones=" + zones;
+        if (cleanThresholdHigh.isPresent()) {
+            ret += ",clean_high=" + cleanThresholdHigh.get();
+        }
+        if (cleanThresholdLow.isPresent()) {
+            ret += ",clean_low=" + cleanThresholdLow.get();
+        }
+        return ret;
     }
 
-    Workload(int chunkSize, int latency, int iterations, int evictThresholdHigh, int evictThresholdLow, String evictionType, int zones) {
+    Workload(int chunkSize, int latency, int iterations, int evictThresholdHigh, int evictThresholdLow, Optional<Integer> cleanThresholdHigh, Optional<Integer> cleanThresholdLow, String evictionType, int zones) {
         this.chunkSize = chunkSize;
         this.latency = latency;
         this.iterations = iterations;
         this.evictThresholdHigh = evictThresholdHigh;
         this.evictThresholdLow = evictThresholdLow;
+        this.cleanThresholdHigh = cleanThresholdHigh;
+        this.cleanThresholdLow = cleanThresholdLow;
         this.evictionType = evictionType;
         this.zones = zones;
     }
