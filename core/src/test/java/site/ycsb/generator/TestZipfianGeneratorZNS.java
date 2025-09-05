92c7a7be183daf34f15c149df3955dc769bf1abd
John Ramsden <john@ubc.ramsdenj.ca>
2025-09-04 17:46:36 -0700

    Add SIGMETRICS bench

--- core/src/test/java/site/ycsb/generator/TestZipfianGeneratorZNS.java
+++ core/src/test/java/site/ycsb/generator/TestZipfianGeneratorZNS.java
@@ -17,8 +17,7 @@ public class TestZipfianGeneratorZNS {
 
 //    final int zone_size = 1077 * 1024 * 1024;
 //    final int num_zones = 100;
-    // final int zone_size = 1077 * 1024 * 1024; // ZNS
-   final int zone_size = 1024 * 1024 * 1024; // SSD
+    final int zone_size = 1077 * 1024 * 1024;
 
     int chunks_in_64K = (int) (zone_size / Math.pow(2, 16));
     int chunks_in_512M = (int) (zone_size / Math.pow(2, 29));
@@ -26,15 +25,24 @@ public class TestZipfianGeneratorZNS {
     final String directory = "./target/workloads/";
     final Workload[] workloads = {
         // Promotional - parameter eval
-        new Workload((int) Math.pow(2, 16), 40632, 9830400, 1, 2, Optional.empty(), Optional.empty(), "promotional", 40),
-        new Workload((int) Math.pow(2, 29), 5413781, 6_000, 8, 16, Optional.empty(), Optional.empty(), "promotional", 200),
-        // Chunk - parameter eval
-        new Workload((int) Math.pow(2, 16), 40632, 9830400, chunks_in_64K, chunks_in_64K*2, Optional.of(chunks_in_64K), Optional.of(chunks_in_64K / 2), "chunk", 40),
-        new Workload((int) Math.pow(2, 29), 5413781, 6_000, chunks_in_512M*8, chunks_in_512M*16, Optional.of(chunks_in_512M*8), Optional.of(chunks_in_512M*4), "chunk", 200),
-        // Promo - GC Eval
-        new Workload((int) Math.pow(2, 28), 0, 6_000, 8, 16, Optional.empty(), Optional.empty(), "promotional", 904),
-        new Workload((int) Math.pow(2, 28), 3209583, 6_000, 8, 16, Optional.empty(), Optional.empty(), "promotional", 904),
+        new Workload((int) Math.pow(2, 16), 40632, 49152000, 1, 2, Optional.empty(), Optional.empty(), "promotional", 904),
+        new Workload((int) Math.pow(2, 28), 3209583, 12_000, 4, 16, Optional.empty(), Optional.empty(), "promotional", 904),
+        new Workload((int) Math.pow(2, 29), 5413781, 6_000, 8, 16, Optional.empty(), Optional.empty(), "promotional", 904),
+        new Workload(zone_size, 11524248, 3_000, 8, 24, Optional.empty(), Optional.empty(), "promotional", 904),
     };
+
+    // Directed study
+//    final Workload[] workloads = {
+//        // Promotional - parameter eval
+//        new Workload((int) Math.pow(2, 16), 40632, 9830400, 1, 2, Optional.empty(), Optional.empty(), "promotional", 40),
+//        new Workload((int) Math.pow(2, 29), 5413781, 6_000, 8, 16, Optional.empty(), Optional.empty(), "promotional", 200),
+//        // Chunk - parameter eval
+//        new Workload((int) Math.pow(2, 16), 40632, 9830400, chunks_in_64K, chunks_in_64K*2, Optional.of(chunks_in_64K), Optional.of(chunks_in_64K / 2), "chunk", 40),
+//        new Workload((int) Math.pow(2, 29), 5413781, 6_000, chunks_in_512M*8, chunks_in_512M*16, Optional.of(chunks_in_512M*8), Optional.of(chunks_in_512M*4), "chunk", 200),
+//        // Promo - GC Eval
+//        new Workload((int) Math.pow(2, 28), 0, 6_000, 8, 16, Optional.empty(), Optional.empty(), "promotional", 904),
+//        new Workload((int) Math.pow(2, 28), 3209583, 6_000, 8, 16, Optional.empty(), Optional.empty(), "promotional", 904),
+//    };
 
     final int[] workingSetRatios = new int[]{10, 2};
 
